8355a1891876ab3f92999e29e66560cc73e1c01d
YOOJS1205 <junsang8921@gmail.com>
2025-01-29 18:22:19 +0900

    useState createRenderer 함수 내부로 이동

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,19 @@
-const App = () => (
-  <div className="app">
-    <Header />
-    <h1 style={{ backgroundColor: "red" }}>Hello, JSX!</h1>
-    <button onClick={() => alert("Clicked!")}>Click Me</button>
-  </div>
-);
+import { useState } from "./libs/jsx/render";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <Header />
+      <h1 style={{ backgroundColor: "red" }}>Hello, JSX!</h1>
+      <div>
+        <p>현재 카운트: {count}</p>
+        <button onClick={() => setCount((prev) => prev + 1)}>증가</button>
+      </div>
+    </div>
+  );
+};
 
 const Header = () => <h1>Hello, JSX!</h1>;
 
